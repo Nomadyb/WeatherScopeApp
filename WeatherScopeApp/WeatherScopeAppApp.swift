0389097c3ad2f663b1ab6845f6657ce8163202b9
Nomadyb <Nomadaeyb@outlook.com>
2024-05-04 12:57:27 +0300

    Initial Commit

---
+++ WeatherScopeApp/WeatherScopeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherScopeAppApp.swift
+//  WeatherScopeApp
+//
+//  Created by Ahmet Emin Yalçınkaya on 4.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherScopeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
